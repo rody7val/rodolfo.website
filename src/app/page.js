fc71f052a0320314ebb77c1e7c9fe6008e9284da
Rodolfo Valguarnera <Rod7val@gmail.com>
2023-08-07 00:21:02 -0300

    init

--- src/app/page.js
+++ src/app/page.js
@@ -6,87 +6,171 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          🇦🇷 Rodolfo Valguarnera
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              src="/mi.jpg"
+              alt="Mi Logo"
+              className={styles.miLogo}
+              width={70}
+              height={70}
               priority
             />
-          </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <p>Estamos en contacto</p>
+      </div> 
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="mailto:ruthwolf@protonmail.com"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            ✉️ Email <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>ruthwolf@protonmail.com</p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="tel:+542923699363"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            📱Teléfono <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>2923699363</p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://instagram.com/r.valguarnera"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
+          <Image
+              src="/instagram.png"
+              alt="ig"
+              width={25}
+              height={25}
+              priority
+            /> Instagram <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            @r.valguarnera
+          </p>
+        </a>
+
+        <a
+          href="https://linkedin.com/in/rvalguarnera"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+          <Image
+              src="/Linkedin.png"
+              alt="in"
+              width={25}
+              height={25}
+              priority
+            /> Linkedin <span>-&gt;</span>
+          </h2>
+          <p>
+            @rvalguarnera
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/rody7val"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+          <Image
+              src="/github.webp"
+              alt="gh"
+              className={styles.imgCard}
+              width={38}
+              height={30}
+              priority
+            /> GitHub <span>-&gt;</span>
+          </h2>
+          <p>@rody7val</p>
+        </a>
+
+        <a
+          href="https://github.com/agora-informatica"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            🔥 Ágora Informática <span>-&gt;</span>
+          </h2>
+          <p>
+            @agora-informatica
+          </p>
+        </a>
+
+        <a
+          href="https://portfolio.rodolfo.website"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            💼 Portfolio <span>-&gt;</span>
+          </h2>
+          <p>
+            Demostraciones
+          </p>
+        </a>
+
+        <a
+          href="/cv.pdf"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            📄 CV <span>-&gt;</span>
+          </h2>
+          <p>
+            Hoja de vida
+          </p>
+        </a>
+      </div>
+
+      <div className={styles.center}>
+        <h2>💳 Wallets</h2>
+      </div> 
+
+      <div className={styles.grid}>
+        <a
+          className={styles.card}
+        >
+          <p>
+            CVU (ARS): <br/>
+            <code>0000139300000000426879</code>
+          </p>
+        </a>
+        <br/>
+        <a
+          className={styles.card}
+        >
+          <p>
+            Bitcoin (BTC): <br/>
+            <code>bc1quwg7nkawp5w4fem0pxeqyy4msurqvn5mmcunvat7yxzdqgjxhfesp7ld4v</code>
           </p>
         </a>
       </div>
